5d78723a30e31b317d5d7a2861b3d21353e420ac
Hanish-Thatikonda <110440891+Hanish-Thatikonda@users.noreply.github.com>
2023-09-01 01:36:04 +0530

    Add files via upload

---
+++ pixabay_fetch.js
@@ -0,0 +1,33 @@
+let btn= document.getElementById("btn");
+let search= document.getElementById("search");
+let second= document.getElementById("second");
+
+btn.addEventListener("click", (e)=>{
+    e.preventDefault();
+    //looping through the div tag and removing all the img tags one by one until div is empty
+    while(second.firstElementChild){
+        second.removeChild(second.firstElementChild);
+    }
+    console.log(search.value);
+   
+        window.fetch(`https://pixabay.com/api/?key=39166637-0c41cc4e2f55dd4a0ab2fc585&q=${search.value}&image_type=photo`)
+    .then((data)=>
+        data.json()
+    )
+    .then((response)=> {
+        let x= response.hits;
+        console.log(x);
+        for(let ele of response.hits){
+          console.log(ele);
+          let a = `<img  src="${ele.previewURL}"/>`
+          console.log(a);
+          second.innerHTML+=a
+        }
+    })
+    
+})
+
+
+
+
+
